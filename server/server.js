ca2eb4db38100e111545ef71081eb2adde9162db
Sibin Sajan <sibin.sajan@realiti.io>
2024-12-15 22:02:31 +0530

    Sibin || mongoDB setup and connection

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server is ready"));
@@ -14,4 +17,4 @@ app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-//pregress 30:31
+//pregress 47:55
