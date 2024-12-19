ac9d0a8c9b955d074982301a988babeaa29bb419
Sibin Sajan <sibin.sajan@realiti.io>
2024-12-20 00:37:55 +0530

    Sibin || Redux and Login

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,30 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-      <Navbar>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand></Navbar.Brand>
-          <Navbar.Toggle></Navbar.Toggle>
-          <Navbar.Collapse> </Navbar.Collapse>
-          <Nav>
-            <Nav.Link>
-              <FaSignInAlt />
-            </Nav.Link>
-            <Nav.Link>
-              <FaSignOutAlt />
-            </Nav.Link>
-          </Nav>
+          <Navbar.Brand as={Link} to="/">
+            MERN AUTH
+          </Navbar.Brand>{" "}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/login">
+                <FaSignInAlt /> Sign In
+              </Nav.Link>
+              <Nav.Link as={Link} to="/register">
+                <FaSignOutAlt /> Sign Up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
   );
 };
+
+export default Header;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import store from "./store.js";
+import { Provider } from "react-redux";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
 );
