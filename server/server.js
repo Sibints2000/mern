f3cad7651051f56f6f88bbb41a9b7b242d125e35
Sibin Sajan <sibin.sajan@realiti.io>
2024-12-19 15:02:31 +0530

    Sibin || Backend completed

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server is ready"));
 
